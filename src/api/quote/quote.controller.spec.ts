3e6c0bbd8af5a6376036f7db16f8956c028a4874
filiafobico <oliveira1eduardo2luis3@gmail.com>
2021-09-13 09:43:59 -0300

    feat: create quote/translate

--- src/api/quote/quote.controller.spec.ts
+++ src/api/quote/quote.controller.spec.ts
@@ -1,5 +1,7 @@
 import { GetRandomQuote } from '@app/get-random-quote';
+import { TranslatePhrase } from '@app/translate-phrase';
 import { IQuote } from '@domain/entities/quote';
+import { GoogleTranslateService } from '@infra/services/google-translate/google-translate.service';
 import { ZenQuoteService } from '@infra/services/zen-quote/zen-quote.service';
 import { Test, TestingModule } from '@nestjs/testing';
 import { QuoteController } from './quote.controller';
@@ -7,24 +9,32 @@ import { QuoteController } from './quote.controller';
 describe('QuoteController', () => {
   let quoteController: QuoteController;
   let getRandomQuote: GetRandomQuote;
+  let translatePhrase: TranslatePhrase;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [QuoteController],
       providers: [
         GetRandomQuote,
+        TranslatePhrase,
         ZenQuoteService,
+        GoogleTranslateService,
         {
           provide: 'AXIOS',
           useValue: {
             get: jest.fn(),
           },
         },
+        {
+          provide: 'GOOGLE_TRANSLATE',
+          useValue: {},
+        },
       ],
     }).compile();
 
     getRandomQuote = module.get<GetRandomQuote>(GetRandomQuote);
     quoteController = module.get<QuoteController>(QuoteController);
+    translatePhrase = module.get<TranslatePhrase>(TranslatePhrase);
   });
 
   it('should be defined', () => {
@@ -41,5 +51,16 @@ describe('QuoteController', () => {
       .mockImplementationOnce(async () => result);
 
     expect(await quoteController.random()).toBe(result);
+  });
+
+  it('should translate a Phrase', async () => {
+    const phrase = 'olá';
+    const translated = 'hello';
+
+    jest
+      .spyOn(translatePhrase, 'execute')
+      .mockImplementationOnce(async () => translated);
+
+    expect(await quoteController.translate({ phrase })).toBe(translated);
   });
 });
