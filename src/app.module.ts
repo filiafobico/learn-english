534d500b0ad6ccfdd6f7f7be36c8082c8981202e
filiafobico <oliveira1eduardo2luis3@gmail.com>
2021-10-02 13:41:32 -0300

    fix: remove unused endpoints

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,16 @@
 import { Module } from '@nestjs/common';
 import { ZenQuoteService } from './infra/services/zen-quote/zen-quote.service';
-import { QuoteController } from './api/quote/quote.controller';
-import { GetRandomQuote } from './app/get-random-quote';
 import { GoogleTranslateService } from './infra/services/google-translate/google-translate.service';
 import axios from 'axios';
 import { TranslationServiceClient } from '@google-cloud/translate';
-import { TranslatePhrase } from './app/translate-phrase';
 import { GetLesson } from './app/get-lesson';
 import { LessonController } from './api/lesson/lesson.controller';
 
 @Module({
   imports: [],
-  controllers: [QuoteController, LessonController],
+  controllers: [LessonController],
   providers: [
     ZenQuoteService,
-    GetRandomQuote,
     {
       provide: 'AXIOS',
       useValue: axios,
@@ -24,7 +20,6 @@ import { LessonController } from './api/lesson/lesson.controller';
       useValue: new TranslationServiceClient(),
     },
     GoogleTranslateService,
-    TranslatePhrase,
     GetLesson,
   ],
 })
